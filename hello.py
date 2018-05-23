f409e3cd74c0f00d4d195a0dcb9bc8317dae1e35
lucdenz <lucdenz@protonmail.com>
2018-05-23 14:15:14 +1000

    Added functional example.

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+a = 2
+b = 3
+
+sum = a + b
+print(sum)
+
+mylist = [a, b]
+
+print(mylist[0])
+
+def lukesfunction(cat):
+    print("I am printing " + cat)
+
+lukesfunction("functioninput")
